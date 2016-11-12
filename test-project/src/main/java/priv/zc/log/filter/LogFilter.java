86367428f514d2ca8d9cb7a124015bca93ec6a59
zongchao <zongchaomail@gmail.com>
2016-11-12 17:02:17 +0800

    加入请求日志

---
+++ test-project/src/main/java/priv/zc/log/filter/LogFilter.java
@@ -0,0 +1,47 @@
+package priv.zc.log.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by zongchao on 16/11/12.
+ */
+public class LogFilter implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String uri = req.getRequestURI();
+        long startTime = System.currentTimeMillis();
+        chain.doFilter(request, response);
+        long timeCost = System.currentTimeMillis() - startTime;
+        log.info("uri:{} time:{} param:{}", uri, timeCost,getParameters(req));
+    }
+
+    public static Map<String,String> getParameters(HttpServletRequest request){
+        Map<String,String> map = new HashMap<String,String>();
+        for(Map.Entry<String,String[]> entry:request.getParameterMap().entrySet()){
+           map.put(entry.getKey(),entry.getValue()[0]);
+        }
+        return map;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
