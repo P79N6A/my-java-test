07dfe532d85b2895a966295ae8d33bab3113e396
zongchao <zongchaomail@gmail.com>
2017-04-07 16:52:52 +0800

    velocity支持测试

--- jfinal/src/main/java/demo/HelloController.java
+++ jfinal/src/main/java/demo/HelloController.java
@@ -4,4 +4,14 @@ public class HelloController extends Controller {
     public void index() {
         renderText("Hello JFinal world.");
     }
+
+    public void jsp(){
+        renderJsp("index.jsp");
+
+    }
+
+    public void vm(){
+        setAttr("name","张三");
+        renderVelocity("index.vm");
+    }
 }
