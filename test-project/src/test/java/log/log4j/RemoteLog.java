c5c89c86027fc367144c9e6c81813aa10a61e289
zongchao <zongchaomail@gmail.com>
2017-04-20 13:32:03 +0800

    logback测试

---
+++ test-project/src/test/java/log/log4j/RemoteLog.java
@@ -0,0 +1,19 @@
+package log.log4j;
+
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+public class RemoteLog {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Properties props = System.getProperties();
+		props.setProperty("log4j.configuration", "test/log4j.remote.properties");
+		Logger log = Logger.getLogger(RemoteLog.class);
+		for (int i = 0; i < 10; i++) {
+			log.debug("test 李恩来 .avi " + i);
+			Thread.sleep(500);
+		}
+	}
+}
